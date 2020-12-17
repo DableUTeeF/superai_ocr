123a1ab5bcc28a15b9ece43f0d6cf6fb3c2e444e
palm <palm22180@gmail.com>
2020-12-17 11:27:18 +0700

    The fixed_crop somewhat works

---
+++ fixed_crop.py
@@ -0,0 +1,77 @@
+import cv2
+import os
+import pytesseract
+import numpy as np
+import easyocr
+reader = None
+
+if __name__ == '__main__':
+    root = 'redstamp'
+    for file in os.listdir(root):
+        # if '131' not in file:
+        #     continue
+
+        raw_image = cv2.imread(os.path.join(root, file))
+
+        image = raw_image.copy()
+        # image[image[..., 2] > 100] = 255
+        # image[image[..., 2] < 100] = 0
+
+        height = image.shape[0]
+        width = image.shape[1]
+        im1 = image[int(height/3.2):int(height/1.75), int(width*0.25):]
+        # cv2.imshow('color', im1)
+        im1[im1[..., 2] > 100] = 255
+        im1[im1[..., 0] > 100] = 255
+        im1[im1[..., 2] < 100] = 0
+        k = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
+        im1 = 255-cv2.morphologyEx(255-im1, cv2.MORPH_CLOSE, k)
+        im1 = 255-cv2.morphologyEx(255-im1, cv2.MORPH_CLOSE, k)
+
+        # tha_id = pytesseract.image_to_string(im1, lang='tha').split('\n')[0]
+        eng_id = pytesseract.image_to_data(im1, output_type='dict')
+        selected_id = ''
+        for i in range(len(eng_id['text'])):
+            word = eng_id['text'][i]
+            if len(word) > len(selected_id):
+                selected_id = word
+        if len(selected_id) == 0:
+            tha_id = pytesseract.image_to_data(im1, lang='tha', output_type='dict')
+            for i in range(len(tha_id['text'])):
+                word = tha_id['text'][i]
+                if len(word) > len(selected_id):
+                    selected_id = word
+        if len(selected_id) == 0:
+            if reader is None:
+                reader = easyocr.Reader(['en'])
+            res = reader.recognize(im1)
+            for result in res:
+                word = result[1]
+                if len(word) > len(selected_id):
+                    selected_id = word
+        print(selected_id)
+
+        # cv2.imshow('1', im1)
+        im2 = image[height//2:int(height/1.25)]
+        cv2.imshow('color2', im2)
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2HSV)
+        low_red = np.array([100, 80, 90])
+        high_red = np.array([130, 255, 255])
+        blue_mask = cv2.inRange(im2, low_red, high_red)
+        im2 = 255-cv2.cvtColor(blue_mask, cv2.COLOR_GRAY2BGR)
+        # im2 = 255-cv2.morphologyEx(im2, cv2.MORPH_CLOSE, k)
+        # im2 = 255-cv2.morphologyEx(255-im2, cv2.MORPH_CLOSE, k)
+        thai_date = pytesseract.image_to_string(im2, lang='tha').split(' ')
+        eng_date = pytesseract.image_to_string(im2).split(' ')
+        selected_day = eng_date[-3]
+        selected_month = thai_date[-2]
+        selected_year = eng_date[-1]
+        print(f'{selected_day} {selected_month} {selected_year}')
+
+        # print(pytesseract.image_to_string(im2, lang='tha').split('\n')[0])
+        # print(pytesseract.image_to_string(im2).split('\n')[0])
+        # print(pytesseract.image_to_string(im2, lang='tha').split('\n')[0])
+        # print(pytesseract.image_to_string(im2).split('\n')[0])
+        print()
+        cv2.imshow('2', im2)
+        cv2.waitKey()
--- recieve_id_rulebase.py
+++ recieve_id_rulebase.py
@@ -1,24 +1,68 @@
 import cv2
 import pytesseract
 import os
+from PIL import Image
+import numpy as np
+import easyocr
+reader = easyocr.Reader(['en'])
 
 if __name__ == '__main__':
     root = 'redstamp'
     for file in os.listdir(root):
-        image = cv2.imread(os.path.join(root, file))
+        # if '196' not in file:
+        #     continue
+
+        raw_image = cv2.imread(os.path.join(root, file))
+        raw_image = raw_image[raw_image.shape[0] // 3:, :]
+        image = raw_image.copy()
         # image = cv2.boxFilter(image, -1, (5, 5))
         image[image[..., 2] > 100] = 255
-        image[image[..., 2] < 100] = 0
+        # image[image[..., 2] < 100] = 0
 
-        image = image[image.shape[0] // 3:, :]
-        cv2.imshow('r', image[..., 2])
-        text = pytesseract.image_to_string(image[..., 2], lang='tha')
+        # pil_image = Image.fromarray(image[..., 2])
+        # pil_image.show()
+        k = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
+        red = 255-cv2.morphologyEx(255-image[..., 2], cv2.MORPH_CLOSE, k)
+        red = 255-cv2.morphologyEx(255-red, cv2.MORPH_CLOSE, k)
+        cv2.imshow('r', red)
+        res = reader.recognize(red)
+        print(res)
+        result = pytesseract.image_to_data(red, lang='tha', output_type=pytesseract.Output.DICT)
         count = 0
-        for char in text:
-            if 47 < ord(char) < 58:
-                print(char, end='')
-                count += 1
+        x1 = float('inf')
+        y1 = float('inf')
+        x2 = -float('inf')
+        y2 = -float('inf')
+        line_num = 100
+        for i in range(len(result['text'])):
+            for char in result['text'][i]:
+                if result['line_num'][i] > line_num:
+                    # print()
+                    break
+                if 47 < ord(char) < 58:
+                    if result['top'][i] + result['height'][i] > y2:
+                        y2 = result['top'][i] + result['height'][i]
+                    # print(char, end='')
+                    if line_num == 100:
+                        line_num = result['line_num'][i]
             if count >= 4:
-                print()
                 break
+        cv2.line(raw_image, (0, y2), (image.shape[1], y2), (0, 255, 0))
+        # pil_image = Image.fromarray(raw_image[..., ::-1])
+        # pil_image.show()
+        cv2.imshow('raw', raw_image)
+        blue = raw_image[y2:, :, 2]
+        blue[raw_image[y2:, :, 2] > 100] = 255
+        blue[raw_image[y2:, :, 2] < 100] = 0
+        # cv2.imshow('blue', blue)
+        # k = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1]], np.uint8)
+        # blue = 255-cv2.morphologyEx(255-blue, cv2.MORPH_CLOSE, k)
+        # blue = 255-cv2.morphologyEx(255-blue, cv2.MORPH_CLOSE, k)
+        text = pytesseract.image_to_string(blue, lang='tha')
+        print(text)
+
+        # pil_image = Image.fromarray(blue)
+        # pil_image.show()
+        cv2.imshow('closed', blue)
         cv2.waitKey()
+        # break
