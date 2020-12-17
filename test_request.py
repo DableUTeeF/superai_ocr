683d566165fdef9eb6021c21c7788656887d9ed1
palm <palm22180@gmail.com>
2020-12-17 13:12:28 +0700

    Now http sends both english and thai date from tesseract

--- test_request.py
+++ test_request.py
@@ -1,5 +1,5 @@
 import requests
 
 resp = requests.post("http://localhost:5000/ocr/redstamp",
-                     files={"file": open('/home/palm/PycharmProjects/ocr/redstamp/doc 133.png', 'rb')})
+                     files={"file": open('/home/palm/PycharmProjects/ocr/redstamp/doc 135.png', 'rb')})
 print(resp.json())
